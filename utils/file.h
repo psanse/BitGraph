ebfa5e1f40dfd051032fef67dd63539a0b4e0516
pablo <pablo.sansegundo@gmail.com>
2025-01-19 13:17:18 +0100

    refactored class File in utils added appropiate unit tests

--- utils/file.h
+++ utils/file.h
@@ -8,8 +8,7 @@
 
 #include <iostream>
 #include <fstream>
-
-using namespace std;
+#include "logger.h"
 
 #define FILE_LOG(file,type)		File( (file), File::type).getFileStream()		
 
@@ -17,54 +16,49 @@ class File{
 public:
 	 enum mode_t {READ=0, WRITE, READ_WRITE, APPEND} ;	 
 
-	 fstream& getFileStream(){
-		return fs;
-	}
+	 //getter
+	 std::fstream& getFileStream(){	return fs; }
 
-	 File(const char* filename, mode_t mode=APPEND){
+	 //contructor
+	 File(const char* filename, mode_t mode = APPEND)  
+	 {
 		 switch(mode){
-		 case READ:
-			 this->type=mode;
-			 fs.open(filename,fstream::in);
+		 case READ:			
+			 fs.open(filename, std::fstream::in);
 			 if(fs.fail()){
-				 cerr<<"unable to open file: "<<filename<<endl;
+				 LOG_ERROR("unable to open reading file: " , filename, "-File::File ");
 			 }
 			 break;
-		 case WRITE:
-			 this->type=mode;
-			 fs.open(filename,fstream::out);
+		 case WRITE:			
+			 fs.open(filename, std::fstream::out);
 			 if(fs.fail()){
-				 cerr<<"unable to open file: "<<filename<<endl;
+				 LOG_ERROR("unable to open writing file: ", filename, "-File::File ");				
 			 }
 			 break;
-		 case READ_WRITE:
-			 this->type=mode;
-			 fs.open(filename,fstream::in|fstream::out);
+		 case READ_WRITE:			
+			 fs.open(filename, std::fstream::in | std::fstream::out);
 			 if(fs.fail()){
-				 cerr<<"unable to open file: "<<filename<<endl;
+				 LOG_ERROR("unable to open reading/writing file: ", filename, "-File::File ");
 			 }
 			 break;
-		 case APPEND:	//Considered only for writing purposes
-			 this->type=mode;
-			 fs.open(filename,fstream::out | fstream::app);
+		 case APPEND:																//Considered only for writing purposes
+			 fs.open(filename, std::fstream::out | std::fstream::app);
 			 if(fs.fail()){
-				 cerr<<"unable to open file: "<<filename<<endl;
+				 LOG_ERROR("unable to open appending file: ", filename, "-File::File ");
 			 }
 			 break;
 
 		 default:
-			 cerr<<"incorret mode for FILE_LOG"<<endl;
+			 LOG_ERROR("unknonw mode for operating on a file -File::File ");			
 		 }
 	}
 
-	~File(void){
-		if(fs.is_open())
-				fs.close();
-	}
+	 //destructor
+	 ~File() { fs.close(); }
+	
 
 private:
-	mode_t type;
-	fstream fs;
+	std::fstream fs;
 };
 
 #endif
