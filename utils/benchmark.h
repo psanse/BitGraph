3bb54236fabac33014bd8752205bd68ae61265d9
pablo <pablo.sansegundo@gmail.com>
2025-01-19 20:56:29 +0100

    refactored the Benchmark Class  in benchmark.cpp refactored the unit tests in test_benchmark.cpp

--- utils/benchmark.h
+++ utils/benchmark.h
@@ -1,48 +1,88 @@
-//benchmark.h interface for Benchmark class, a wrapper to manage graph instances
-//author:pablo san segundo
+/**
+ * @file benchmark.h
+ * @brief interface for Benchmark class, a wrapper to manage graph datasets for testing
+ * @date ?
+ * @last_update 17/01/25
+ * @author pss
+ **/
 
 #ifndef __BENCHMARK_H__
 #define __BENCHMARK_H__
 
 #include <iostream>
+#include <string>
 #include <vector>
 #include <map>
 
-using namespace std;
+//aliases
+using vstr_t = std::vector<std::string>;
+using mstri_t = std::map<std::string, int>;
+
+///////////////////
+//
+// Benchmark class
+//
+//////////////////
 
 class Benchmark{
-	friend ostream& operator <<(ostream&, Benchmark& b);
+	friend std::ostream& operator<<			(std::ostream& o, Benchmark& b)	 { return b.print(o); }
 
-protected:	//cannot be instantiated directly
-	Benchmark(const string& pathName);		
+	//move semantics and copy semantics disallowed
+	Benchmark				(const Benchmark& b) = delete;
+	Benchmark& operator=	(const Benchmark& b) = delete;
+	Benchmark				(Benchmark&& b)		 = delete;
+	Benchmark& operator=	(Benchmark&& b)		 = delete;
+
+protected:	
+	//constructor (protected) - cannot be instantiated 
+explicit Benchmark(const std::string& pathName);			
+	
+public:
+	//destructor
 	virtual ~Benchmark(){}
 
-public:
 ///////////
 // setters and getters
-	string get_path_name				()						{return m_PATH;}
-	void setArrayOfFilenames			(vector<string> list)	{m_lf=list;}
-	void clear_list_of_names			()						{m_lf.clear(); m_mf.clear();}
-	vector<string> getArrayOfFilenames	()						{return m_lf;}
-	map<string, int> getMapOfFilenames	()						{return m_mf;}
-	int number_of_instances				()						{return m_lf.size();}
+		
+	int number_of_instances					()									{return lf_.size();}
+
+	void setArrayOfFilenames				(std::vector<std::string> list)		{ lf_ = list; }
 	
-	int get_Value						(string);					
+	int get_value							(std::string filename);
+	std::string get_path					()									{ return PATH_; }
+	vstr_t& getArrayOfFilenames				()									{ return lf_; }
+	mstri_t& getMapOfFilenames				()									{ return mf_; }
+
 /////////////
-// E/S
-	void print(ostream& = cout) ;
+// Context
+	void clear_instances					()									{ lf_.clear(); mf_.clear(); }
+
 
 //////////////
-//Benchmarks (graph)
-	virtual void add_test(const string, int val);
-	virtual void add_test(const string);
+//Benchmark operations
+
+	/**
+	* @brief Adds a test with a value to the benchmark list
+	* **/
+	virtual void add_test					(const std::string, int val);
+
+	/**
+	* @brief Adds a test without value to the benchmark list
+	* **/
+	virtual void add_test					(const std::string);
+
+
+/////////////
+// I/O
+
+	std::ostream& print						(std::ostream & = std::cout);
 	
 ////////////
 //Data members
 protected:
-	const string		m_PATH;		//fixed path for all files
-	vector<string>		m_lf;		//path + filename 
-	map<string, int>	m_mf;		//(path + filename) --> value
+	const std::string		PATH_;		//FIXED path for all filenames
+	vstr_t					lf_;		//list of filemanes (PATH_ + name) 
+	mstri_t					mf_;		//mapping filenames -> value
 };
 
 #endif
--- utils/common.cpp
+++ utils/common.cpp
@@ -1,6 +1,10 @@
-//common.cpp: implementation of common.h  - common utilities for the framework e
-//
-//@created: 2024/10/31 
+/**
+ * @file common.cpp
+ * @brief implementation of common utilities in common.h
+ * @date 31/10/2024 (before the implementation was in the header file)
+ * @last_update 17/01/25
+ * @author pss
+ **/
 
 #include "common.h"
 
