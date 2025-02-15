e6e52cc15424191fa8aa0e3d3070c64a1ae33322
pablo <pablo.sansegundo@gmail.com>
2025-02-15 08:58:00 +0100

    minor comments

--- bitscan/bbscan_sparse.h
+++ bitscan/bbscan_sparse.h
@@ -68,13 +68,8 @@ virtual	 inline int popcn64			(int nBit)	const;							//population size from (an
 
 //////////////////
 /// data members
-public:
-	//struct scan_t{																			//Cache memory for bitscanning optimization
-	//	scan_t():bbi(EMPTY_ELEM), pos(MASK_LIM){}
-	//	int bbi;	//bitboard index in the collection (not in the bitstring) 	
-	//	int pos;	//bit position for bitscan		
-	//};
 
+public:
 	 scan_t m_scan;
 };
 
