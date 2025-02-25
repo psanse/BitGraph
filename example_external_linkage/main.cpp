c49a4ea37e1200472b72c0001092709fb74b93c5
pablo <pablo.sansegundo@gmail.com>
2025-02-26 00:39:19 +0100

    removed print_edges from external linking example

--- example_external_linkage/main.cpp
+++ example_external_linkage/main.cpp
@@ -8,16 +8,16 @@ int main() {
 	g.add_edge(0, 1);
 	g.add_edge(0, 2);
 	g.print_data();
-	g.print_edges();
+	
 	
 	cout << "****************************" << endl;
 
-	//sparse_graph with 1M vertices
-	sparse_ugraph sg(1000000);
+	//sparse_graph with 10 million vertices
+	sparse_ugraph sg(10000000);
 	sg.add_edge(0, 1);
 	sg.add_edge(0, 2);
 	sg.print_data();
-	sg.print_edges();
+	
 
 	cout << "****************************" << endl;
 
