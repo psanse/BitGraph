8db6e463063f2e547994e5774fe03399fe3a71b1
pablo <pablo.sansegundo@gmail.com>
2025-02-25 20:24:15 +0100

    external linkage example modified - check

--- example_external_linkage/main.cpp
+++ example_external_linkage/main.cpp
@@ -2,12 +2,24 @@
 #include <graph/graph.h>
 
 int main() {
+
+	//non-sparse graph with 10 vertices
 	ugraph g(10);
 	g.add_edge(0, 1);
 	g.add_edge(0, 2);
 	g.print_data();
 	g.print_edges();
+	
+	cout << "****************************" << endl;
 
+	//sparse_graph with 1M vertices
+	sparse_ugraph sg(1000000);
+	sg.add_edge(0, 1);
+	sg.add_edge(0, 2);
+	sg.print_data();
+	sg.print_edges();
+
+	cout << "****************************" << endl;
 
 
 	return 0;
