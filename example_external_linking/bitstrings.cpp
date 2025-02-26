41ccd34763b733eee511e71751116ad7773c6f6b
pablo <pablo.sansegundo@gmail.com>
2025-02-26 19:04:30 +0100

    adding targets to the external linking example

---
+++ example_external_linking/bitstrings.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bitscan/bitscan.h>
+
+int main() {
+
+	//small graph with 10 vertices
+	ugraph g(10);
+	g.add_edge(0, 1);
+	g.add_edge(0, 2);
+	g.print_data();
+	g.print_edges();
+	
+	cout << "****************************" << endl;
+
+	//massive graph with 50 million vertices
+	sparse_ugraph sg(50000000);
+	sg.add_edge(0, 1);
+	sg.add_edge(0, 2);
+	sg.print_data();
+	sg.print_edges();
+	
+	cout << "****************************" << endl;
+
+	//make a copy of the massive graph
+	sparse_ugraph sgCOPY(sg);
+	sgCOPY.print_data();
+	sgCOPY.print_edges();
+
+	cout << "****************************" << endl;
+
+	return 0;
+}
