ec971743b32e5b729f0d48a4b780509d74b7da53
pablo <pablo.sansegundo@gmail.com>
2025-03-25 20:25:11 +0100

    cleaned project code which had somehow been synchro

--- src/bitscan/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Pablo/Desktop/BitGraph/bitscan/tests
-# Build directory: C:/Users/Pablo/Desktop/BitGraph/bitscan/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/utils/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Pablo/Desktop/BitGraph/utils/tests
-# Build directory: C:/Users/Pablo/Desktop/BitGraph/utils/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
