03c1c53b51830b7a1d7423a739e49082c5c5d83d
pablo <pablo.sansegundo@gmail.com>
2025-07-19 17:38:44 +0200

    removed Thread class in thread.h

--- src/utils/tests/test_thread.cpp
+++ src/utils/tests/test_thread.cpp
@@ -13,55 +13,6 @@
 
 using namespace bitgraph;
 
-class ThreadExample{
-	
-public:
-	ThreadExample(int d):res_(d){};
-		
-	/**
-	* @brief Mock task to be run by the thread
-	**/
-	int task_mul_by_2 (){
-			
-		//task
-		res_ *= 2;
-
-		//sleep for 0.2s
-		std::this_thread::sleep_for(std::chrono::milliseconds(200));
-
-		//LOG_DEBUG("Thread finished");
-
-		return 0;
-	};
-
-	
-	/**
-	* @brief driver to execute member function  task_mul_by_2 in a new thread
-	*		 and wait for the thread to finish
-	* @returns 0 if successful, -1 otherwise (non-throwing interface)
-	**/
-	int Start(double tout_ms){
-		using Tf = int(ThreadExample::*)();
-		int retVal = Thread<ThreadExample, Tf>::Start(&ThreadExample::task_mul_by_2, this);
-		return retVal;
-	}
-
-//////////
-	int res_;
-};	
-
-TEST(Thread, basic){
-	
-	ThreadExample t(5);
-	int retVal= t.Start(1000);
-	EXPECT_EQ(0, retVal);			//finished correctly
-
-	//LOG_DEBUG("passed through here");
-
-	//retVal = t.Start(4000);
-	//EXPECT_EQ(0, retVal);			//finished correctly
-
-}
 
 TEST(run_thread, callable) {
 
@@ -72,7 +23,8 @@ TEST(run_thread, callable) {
 	};
 
 	//////////////////////////////////////
-	EXPECT_EQ(run_thread(functor()), 0);
+	functor f;
+	run_thread(f);
 	//////////////////////////////////////
 }
 
@@ -88,6 +40,60 @@ TEST(run_thread, member_function) {
 
 	X x;
 	//////////////////////////////////////
-	EXPECT_EQ(run_thread(&X::run, x), 0);
+	run_thread(&X::run, x);
 	//////////////////////////////////////
 }
+
+//////////////////////////
+// CHECK THIS CODE (19/07/25)
+
+//class ThreadExample final{
+//	
+//public:
+//	ThreadExample(int d):res_(d){};
+//		
+//	/**
+//	* @brief Mock task to be run by the thread
+//	**/
+//	int task_mul_by_2 (){
+//			
+//		//task
+//		res_ *= 2;
+//
+//		//sleep for 0.2s
+//		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+//
+//		//LOG_DEBUG("Thread finished");
+//
+//		return res_;
+//	};
+//
+//	
+//	/**
+//	* @brief driver to execute member function task_mul_by_2 in a new thread
+//	*		 and wait for the thread to finish
+//	* @returns result of task_mul_by_2
+//	**/
+//	int Start(double tout_ms){
+//		using Tf = int(ThreadExample::*)();
+//		Thread<ThreadExample, Tf>::Start(&ThreadExample::task_mul_by_2, this);
+//		return res_;
+//	}
+//
+//private:
+////////////
+//	int res_;
+//};	
+
+//TEST(Thread, basic){
+//	
+//	ThreadExample t(5);
+//	int retVal= t.Start(1000);
+//	EXPECT_EQ(10, retVal);			//finished correctly
+//
+//	//LOG_DEBUG("passed through here");
+//
+//	//retVal = t.Start(4000);
+//	//EXPECT_EQ(0, retVal);			//finished correctly
+//
+//}
--- src/utils/thread.h
+++ src/utils/thread.h
@@ -1,6 +1,7 @@
 /**
 * @file thread.h
-* @brief convenience functions and classed to run callaeble objects or member functions as threads
+* @brief convenience functions and classed to run callable objects or member functions as threads.
+* 		 Application: algorithm test framework - algoritms are run sequentially  and main thread waits for each one to finish
 * @details: created 2013, last_update 19/07/2025
 * @dev: pss
 **/ 
@@ -14,87 +15,60 @@
 namespace bitgraph {
 
 	namespace com {
-
-		////////////////////
-		//
-		// Thread class
-		//
-		// Runs a function member T_func without params of a class T as a new thread
-		// (main thread waits for its finish)
-		//
-		//////////////////
-		template <class T, class T_func>
-		class Thread {
-
-		public:
-			/**
-			* @ brief Starts a thread with a function member of a class
-			* @returns 0 if successful, -1 otherwise
-			**/
-			static int Start(T_func f, T* pthis) {
-
-				try {
-					/////////////////////////
-					std::thread t(f, pthis);
-					//////////////////////////
-
-					//waits for the thread to finish
-					t.join();
-
-				}
-				catch (...) {
-					LOG_ERROR("Error during thread execution - Thread::Start");
-					return -1;
-				}
-
-				return 0;
-			}
-
-		};//end class Thread
-
 	
 		/**
 		* @brief: Runs a member function of an object with generic params as a new thread and waits for its finish
-		* returns 0 if successful, -1 otherwise (non-throwing interface)	
 		**/
 		template<typename func_t, typename obj_t, typename... Args>
-		int run_thread(func_t func, obj_t& obj, Args&&... args) noexcept {
+		void run_thread(func_t func, obj_t& obj, Args&&... args) noexcept {
 
-			try {
-				std::thread t(func, std::ref(obj), std::forward<Args>(args)...);
-				t.join();															// Waits for the thread to finish	
-			}
-			catch (...) {
-				LOG_ERROR("Error during thread execution - run_thread");
-				return -1;
-			}
-
-			return 0;
+			std::thread t(func, std::ref(obj), std::forward<Args>(args)...);
+			t.join();															// Waits for the thread to finish	
 		}
 
 		/**
 		* @brief: Runs a callable object as a thread with generic params and waits for its finish
-		* returns 0 if successful, -1 otherwise (non-throwing interface)
 		**/
 		template<typename callable_t, typename... Args>
-		int run_thread(callable_t functor, Args&&... args) noexcept {
+		void run_thread(callable_t functor, Args&&... args) noexcept {
 			
-			try {
-				std::thread t(functor, std::forward<Args>(args)...);
-				t.join();															// Waits for the thread to finish
-			}
-			catch (...) {
-				LOG_ERROR("Error during thread execution - run_thread");
-				return -1;
-			}
-
-			return 0;
+			std::thread t(functor, std::forward<Args>(args)...);
+			t.join();															// Waits for the thread to finish
 		}
+
+
+		//////////////////////
+		////
+		//// Thread class
+		////
+		//// Runs a function member T_func without params of a class T as a new thread
+		//// (main thread waits for its finish)
+		////
+		////////////////////
+		//template <class T, class T_func>
+		//class Thread {
+
+		//public:
+		//	/**
+		//	* @ brief Starts a thread with a function member of a class
+		//	**/
+		//	static void Start(T_func f, T* pthis) {
+		//						
+		//		/////////////////////////
+		//		std::thread t(f, pthis);
+		//		//////////////////////////
+
+		//		//waits for the thread to finish
+		//		t.join();
+		//		
+		//	}
+
+		//};
 
 
 	}//end namespace com
 	
-	using com::Thread;
+	//using com::Thread;
 	using com::run_thread;
 
 
