714d817e65cb3a97850273f11779176e7f641d77
pablo.sansegundo <pablo.sansegundo@upm.es>
2025-09-22 10:48:46 +0200

    first commit

---
+++ src/utils/utils.h
@@ -0,0 +1,7 @@
+/**
+ * @file utils.
+ * @brief implementation of common utilities in common.h
+ * @date 31/10/2024 (before the implementation was in the header file)
+ * @last_update 17/01/25
+ * @author pss
+ **/
