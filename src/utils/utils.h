eb985bc3836d76a5968320f1ab802e0d448a5b26
pablo.sansegundo <pablo.sansegundo@upm.es>
2025-09-22 11:17:14 +0200

    added utils.h unique header file for BITGRAPH utils

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,7 +1,22 @@
 /**
- * @file utils.
- * @brief implementation of common utilities in common.h
- * @date 31/10/2024 (before the implementation was in the header file)
- * @last_update 17/01/25
- * @author pss
- **/
+ * @file utils.h
+ * @brief one and only header file for BITGRAPH utilities
+ * @details: created (22/09/2025)
+ * @developer: pss
+**/
+
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include "batch.h"
+#include "benchmark.h"
+#include "common.h"
+#include "file.h"
+#include "info/info_base.h"
+#include "common_types.h"
+#include "logger.h"
+#include "prec_timer.h"
+#include "task.h"
+
+
+#endif // !_UTILS_H_
