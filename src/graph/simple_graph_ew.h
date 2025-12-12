54244b3a59286176fd9f74443c34dac19e1dc9b6
pablo <pablo.sansegundo@gmail.com>
2025-12-12 11:13:31 +0100

    fix g++

--- src/graph/simple_graph_ew.h
+++ src/graph/simple_graph_ew.h
@@ -507,9 +507,9 @@ namespace bitgraph {
 		/////////////
 		//useful interface for graph operations (no weights)
 
-		int max_graph_degree()						const { return this->g_.max_graph_degree(); }
-		int degree(int v)							const { return this->g_.degree(v); }
-		int degree(int v, const _bbt& bbn)			const { return this->g_.degree(v, bbn); }
+		int max_graph_degree()								const { return this->g_.max_graph_degree(); }
+		int degree(int v)									const { return this->g_.degree(v); }
+		int degree(int v, const typename _bbt& bbn)			const { return this->g_.degree(v, bbn); }
 
 		/////////////
 		//other operations
